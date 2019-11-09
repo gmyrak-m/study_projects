33382e8eb8b7321c5f4ad6dad8c15cb956aaab54
mariya.gmyrak@gmail.com <mariya.gmyrak@gmail.com>
2019-11-09 16:25:41 +0200

    Initial commit.

---
+++ Матрица достижимости/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Матрица_достижимости
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool[,] S = new bool[1,1];
+            bool init = false;
+            int RANGE = 0;
+            int n = 0;
+
+            foreach (string line in File.ReadLines("Смежность.txt"))
+            {
+                if (!init)
+                {
+                    RANGE = line.Length;
+                    init = true;
+                    S = new bool[RANGE, RANGE];
+                }
+
+                for(int m=0; m< RANGE; m++)
+                {
+                    S[n, m] = line[m] == '1';
+                }
+                n++;
+            }
+
+            string[] output = new string[RANGE];
+
+            for(int i=0; i<RANGE; i++)
+            {
+                string line = "";
+                for(int j=0; j<RANGE; j++)
+                {
+                    line += " " + S[i, j];
+                }
+                output[i] = line;
+
+            }
+            File.WriteAllLines("Достижимость.txt", output);
+        }
+    }
+}
---
+++ ромб/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ромб
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            const int center = 40;
+            const int hight = 14;
+
+            for (int i = 0; i < hight; i++) starLine(center - i, center + i);
+            for (int i = hight; i >= 0; i--) starLine(center - i, center + i);
+            //starLine(2, 4);
+
+
+        }
+
+
+        static void starLine(int a, int b)
+        {
+            for (int i = 0; i < a; i++) Console.Write(" ");
+            for (int i = 0; i < b-a+1; i++) Console.Write("*");
+            Console.WriteLine();
+           
+        }
+    }
+} 
+
+        
